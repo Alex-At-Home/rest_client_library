37792d76458311dac4400024ca8278242bdbbaab
Alex Piggott <Alex Piggott>
2016-11-06 17:54:43 -0500

    Complete coverage of JSON building DSL (fix JS/JVM test result difference)

--- rest_scala_core/shared/src/test/scala/org/elastic/rest/scala/driver/utils/SimpleObjectDescriptionTests.scala
+++ rest_scala_core/shared/src/test/scala/org/elastic/rest/scala/driver/utils/SimpleObjectDescriptionTests.scala
@@ -117,8 +117,8 @@ object SimpleObjectDescriptionTests extends TestSuite {
       Try { any2Str(WontWork()) }.isFailure ==> true
 
       // Check  the number combos:
-      any2Str("a", 1.0.toFloat, isFirst = true).trim() ==> """ "a":  1.0 """.trim()
-      any2Str("a", 1.0, isFirst = true).trim() ==> """ "a":  1.0 """.trim()
+      any2Str("a", 1.1.toFloat, isFirst = true).trim() ==> """ "a":  1.1 """.trim()
+      any2Str("a", 1.1, isFirst = true).trim() ==> """ "a":  1.1 """.trim()
       any2Str("a", 1.toLong, isFirst = true).trim() ==> """ "a":  1 """.trim()
       any2Str("a", 1, isFirst = true).trim() ==> """ "a":  1 """.trim()
 
