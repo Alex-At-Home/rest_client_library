c932874ff8eca941b1524e22e80c820358bbee5b
Alex Piggott <Alex Piggott>
2016-12-03 15:06:33 -0500

    Add util for building value class constants (improve type handling)

--- rest_scala_core/shared/src/main/scala/org/elastic/rest/scala/driver/RestBase.scala
+++ rest_scala_core/shared/src/main/scala/org/elastic/rest/scala/driver/RestBase.scala
@@ -51,7 +51,7 @@ object RestBase {
   /** Constants used in value class helpers */
   object ToStringAnyVal {
     /** Placeholder that gets filled in by the `@Constant` annotation */
-    def AutoGenerate[T] = null.asInstanceOf[T]
+    def AutoGenerate[T <: ToStringAnyVal[_]] = null.asInstanceOf[T]
   }
 
   /** Parent type for Modifiers to resources (representing URL parameters)
